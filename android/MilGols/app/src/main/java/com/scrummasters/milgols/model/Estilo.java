675b1a73b503572d97aaa1b54aeeff818c0f2580
Tiago Naves <tiagoufg@gmail.com>
2015-09-30 10:15:29 -0300

    [Android] Implementar registro local de gol

---
+++ android/MilGols/app/src/main/java/com/scrummasters/milgols/model/Estilo.java
@@ -0,0 +1,28 @@
+package com.scrummasters.milgols.model;
+
+import java.util.Date;
+
+/**
+ * Created by tiago on 30/09/15.
+ */
+public class Estilo {
+
+    private Long id;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
